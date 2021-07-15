b11befa3a23a59df0ce4fb38971e9f461adc2f7f
Zulfikar Ali Zaki <81583805+Zulfikar1211@users.noreply.github.com>
2021-07-15 21:27:59 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,58 +1,142 @@
-<!DOCTYPE html>
-<html>
+<?php include 'header.php'; ?>
 
-<head>
-    <title>SISTEM INFORMASI LAUNDRY ZAZ</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-    <script type="text/javascript" src="assets/js/jquery.js"></script>
-    <script type="text/javascript" src="assets/js/bootstrap.js"></script>
-    <link rel="icon" type="image/png" href="icon.png">
-</head>
+<?php
+include '../koneksi.php';
+?>
 
-<body style="background: #C0C0C0">
-    <br />
-    <br />
-    <center>
-        <h2>SISTEM INFORMASI LAUNDRY ZAZ</h2>
-    </center>
-    <br />
-    <br />
+<div class="container">
+    <div class="alert alert-info text-center">
+        <h4 style="margin-bottom: 0px">SELAMAT DATANG DI <b>LAUNDRY ZAZ </b></h4>
+    </div>
 
-    <div class="container">
-        <div class="col-md-4 col-md-offset-4">
-            <?php
-            session_start();
-            if (isset($_GET['pesan'])) {
-                if ($_GET['pesan'] == "gagal") {
-                    echo "<div class='alert alert-danger'>Login gagal! username dan password salah!</div>";
-                } else if ($_GET['pesan'] == "logout") {
-                    echo "<div class='alert alert-info'>Anda telah berhasil logout</div>";
-                } else if ($_GET['pesan'] == "belum_login") {
-                    echo "<div class='alert alert-danger'>Anda harus login untuk mengakses halaman admin</div>";
-                }
-            }
-            ?>
+    <div class="panel">
+        <div class="panel-heading">
+            <h4>Dashboard</h4>
+        </div>
+        <div class="panel-body">
 
-            <form action="login.php" method="post">
-                <div class="panel">
-                    <br />
-                    <div class="panel-body">
-                        <div class="form-group">
-                            <label>Username</label>
-                            <input type="text" class="form-control" name="username">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h1>
+                                <i class="glyphicon glyphicon-user"></i>
+                                <span class="pull-right">
+                                    <?php
+                                    $pelanggan = mysqli_query($koneksi, "select * from pelanggan");
+                                    echo mysqli_num_rows($pelanggan);
+                                    ?>
+                                </span>
+                            </h1>
+                            Jumlah Pelanggan
                         </div>
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input type="password" class="form-control" name="password">
-                        </div>
-                        <input type="submit" class="btn btn-primary" value="Log In">
                     </div>
-                    <br />
                 </div>
-            </form>
+
+                <div class="col-md-3">
+                    <div class="panel panel-warning">
+                        <div class="panel-heading">
+                            <h1>
+                                <i class="glyphicon glyphicon-retweet"></i>
+                                <span class="pull-right">
+
+                                    <?php
+                                    $proses = mysqli_query($koneksi, "select * from transaksi where transaksi_status='0'");
+                                    echo mysqli_num_rows($proses);
+                                    ?>
+                                </span>
+                            </h1>
+                            Jumlah Cucian Di Proses
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <h1>
+                                <i class="glyphicon glyphicon-info-sign"></i>
+                                <span class="pull-right">
+
+                                    <?php
+                                    $proses = mysqli_query($koneksi, "select * from transaksi where transaksi_status='1'");
+                                    echo mysqli_num_rows($proses);
+                                    ?>
+                                </span>
+                            </h1>
+                            Jumlah Cucian Siap Ambil
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">
+                            <h1>
+                                <i class="glyphicon glyphicon-ok-circle"></i>
+                                <span class="pull-right">
+
+                                    <?php
+                                    $proses = mysqli_query($koneksi, "select * from transaksi where transaksi_status='2'");
+                                    echo mysqli_num_rows($proses);
+                                    ?>
+                                </span>
+                            </h1>
+                            Jumlah Cucian Selesai
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
-</body>
 
-</html>
+    <div class="panel">
+        <div class="panel-heading">
+            <h4>Riwayat Transaksi Terakhir</h4>
+        </div>
+        <div class="panel-body">
+            <table class="table table-bordered table-striped">
+                <tr>
+                    <th width="1%">No</th>
+                    <th>Invoice</th>
+                    <th>Tanggal</th>
+                    <th>Pelanggan</th>
+                    <th>Berat (Kg)</th>
+                    <th>Tgl. Selesai</th>
+                    <th>Harga</th>
+                    <th>Status</th>
+                </tr>
+
+                <?php
+                $data = mysqli_query($koneksi, "select * from pelanggan,transaksi where transaksi_pelanggan=pelanggan_id order by transaksi_id desc limit 10");
+                $no = 1;
+                while ($d = mysqli_fetch_array($data)) {
+                ?>
+                    <tr>
+                        <td><?php echo $no++; ?></td>
+                        <td>INVOICE-<?php echo $d['transaksi_id']; ?></td>
+                        <td><?php echo $d['transaksi_tgl']; ?></td>
+                        <td><?php echo $d['pelanggan_nama']; ?></td>
+                        <td><?php echo $d['transaksi_berat']; ?></td>
+                        <td><?php echo $d['transaksi_tgl_selesai']; ?></td>
+                        <td><?php echo "Rp. " . number_format($d['transaksi_harga']) . " ,-"; ?></td>
+                        <td>
+                            <?php
+                            if ($d['transaksi_status'] == "0") {
+                                echo "<div class='label label-warning'>PROSES</div>";
+                            } else if ($d['transaksi_status'] == "1") {
+                                echo "<div class='label label-info'>DICUCI</div>";
+                            } else if ($d['transaksi_status'] == "2") {
+                                echo "<div class='label label-success'>SELESAI</div>";
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
